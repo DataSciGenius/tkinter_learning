25fa74c0872811ff3cdc2f8882d384d678064334
DataSciGenius <datascigenius@gmail.com>
2024-08-04 16:38:00 +0200

    Using Icons, Images, and Exit Buttons

--- 06_images.py
+++ 06_images.py
@@ -4,12 +4,26 @@
 
 # Importing the Tkinter library
 from tkinter import *
+# Importing the Image and ImageTk classes from the PIL (Pillow) library
+from PIL import ImageTk, Image
 
 # Creating the main window (root widget)
 root = Tk()
 root.title("Simple Calculator")
+# Setting the window icon (ensure the path to the icon is correct)
+root.iconbitmap(r'C:\Users\musta\tkinter_project')
 
+# Loading an image from a file and creating an ImageTk object
+my_img = ImageTk.PhotoImage(Image.open('world.jpg'))
+# Creating a Label widget to display the image
+my_label = Label(image=my_img)
+# Displaying the Label widget on the screen
+my_label.pack()
 
+# Creating a Button widget to exit the program
+button_exit = Button(root, text='Exit Program', command=root.quit)
+# Displaying the Button widget on the screen
+button_exit.pack()
 
 # Running the main event loop
 root.mainloop()
